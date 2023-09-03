7c34f7cf5ab003af4316c9ae4c5151eafee4d423
nicolas gomez <nicolas.gomez@correo.ucu.edu.uy>
2023-09-03 07:25:21 -0300

    Cambios comentarios

--- Ejercicio 1/src/Library/Book.cs
+++ Ejercicio 1/src/Library/Book.cs
@@ -8,21 +8,21 @@ namespace Ucu.Poo.Expert
         public string Title { get ; }                           //Contiene la responsabilidad de Conocer el título del libro
         public string Author { get ; }                          // El autor del libro
         public string Code { get ;  }                            // Y el código identificador del libro
-        public string LibrarySector { get ; set; }              // También tiene la responsabilidad de conocer el sector en el que se encuentra
-        public string LibraryShelve { get ; set; }              // Y la estantería en la que está guardado.
+        public string LibrarySector { get ; set; }              // También tiene la responsabilidad y la posibilidad de conocer y cambiar el sector de la librería en el que se encuentra
+        public string LibraryShelve { get ; set; }            // Y la estantería en la que está guardado.
 
         public Book(String title, String author, String code)
         {
             this.Title = title;
             this.Author = author;
-            this.Code = code;                                   //En este caso, la Clase "Book" contiene dos razones para cambiar.
+            this.Code = code;                                   //En este caso, la Clase "Book" contiene una única razón para cambiar
         }
 
-        /*public void ShelveBook(String sector, String shelve)
+        public void ShelveBook(String sector, String shelve)
         {
-            this.LibrarySector = sector;                        //Por lo tanto no cumpliría con SRP.
-            this.LibraryShelve = shelve;
-        }
-        */
-    }
-}
+            this.LibrarySector = sector;                        // Mas no una única responsabilidad
+            this.LibraryShelve = shelve;                        // Bajo mi comprensión, la lase Book cumple con el principio SRP
+        }                                                       // De igual manera, si queremos dividir de mejor manera las responsabilidades
+                                                                // Podríamos crear otra clase que sea específicamente para almacenar
+    }                                                           // y cambiar el sector de la librería y la estantería en la que se encuentra el libro
+}                                                               
