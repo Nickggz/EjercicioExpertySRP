45d1ac19b4640e85a3d29a5c12f66ecca7c2d960
nicolas gomez <nicolas.gomez@correo.ucu.edu.uy>
2023-09-04 07:54:22 -0300

    Cambios

--- Ejercicio 2/src/Program/Program.cs
+++ Ejercicio 2/src/Program/Program.cs
@@ -7,10 +7,10 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            string appointmentResult = AppointmentService.CreateAppointment("Steven Jhonson","55", "986782342", "5555-555-555", DateTime.Now, "Wall Street", "Armand", " ");
+            string appointmentResult = AppointmentService.CreateAppointment("Ralf Manson","55", "986782342", "5555-555-555", DateTime.Now, "Wall Street", "Armand", "Neurologo");
             Console.WriteLine(appointmentResult);
 
-            string appointmentResult2 = AppointmentService.CreateAppointment("Ralf Manson", "56","098542566", "5555-555-555", DateTime.Now, "Queen Street","Armando", "Neurologist");
+            string appointmentResult2 = AppointmentService.CreateAppointment("", "56","098542566", "5555-555-555", DateTime.Now, "Queen Street","Armando", "");
             Console.WriteLine(appointmentResult2);
         }
     }
